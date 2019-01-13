6a71757a18b213b21250e8cd89122fe06d35c6f8
P. Mutale <peter@mutale.nl>
2019-01-13 11:43:11 +0100

    Add settings and requirements

---
+++ settings/environ/dev.py
@@ -0,0 +1,11 @@
+from settings.environ.base import *
+
+ALLOWED_HOSTS = ['localhost', ]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
