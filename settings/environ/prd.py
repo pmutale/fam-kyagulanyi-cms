ca68eed7c21affa1c868ed193a2f79fe3cf881ca
P. Mutale <peter@mutale.nl>
2019-01-31 14:14:50 +0100

    Added Allowed Hosts

--- settings/environ/prd.py
+++ settings/environ/prd.py
@@ -3,6 +3,8 @@ import dj_database_url
 
 DEBUG = False
 
+ALLOWED_HOSTS = ['kirumba.herokuapp.com', ]
+
 DATABASES = {'default': dj_database_url.config(default=os.environ.get('POSTGRES_URL'))}
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static/assets")
