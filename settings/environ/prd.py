dc14f09c46822d6a22e8bae71cb3a7f5ffe9b3b6
P. Mutale <peter@mutale.nl>
2019-02-01 17:50:24 +0100

    Added Heroku Settings

--- settings/environ/prd.py
+++ settings/environ/prd.py
@@ -16,4 +16,4 @@ DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql_psycopg2'
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static/assets")
 
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
