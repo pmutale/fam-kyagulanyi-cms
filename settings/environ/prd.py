40a8345f89d264d07d7c5513ab312cb25c2ad1ec
P. Mutale <peter@mutale.nl>
2019-02-01 23:09:46 +0100

    Added Webpack endpoint

--- settings/environ/prd.py
+++ settings/environ/prd.py
@@ -5,21 +5,21 @@ import dj_database_url
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['kirumba.herokuapp.com', '127.0.0.1' ]
+ALLOWED_HOSTS = ["kirumba.herokuapp.com", "127.0.0.1" ]
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get('POSTGRES_URL'), conn_max_age=600, ssl_require=True),
+    "default": dj_database_url.config(default=os.environ.get("POSTGRES_URL"), conn_max_age=600, ssl_require=True),
 }
 
-DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql_psycopg2'
+DATABASES["default"]["ENGINE"] = "django.db.backends.postgresql_psycopg2"
 
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 WEBPACK_LOADER = {
-    'DEFAULT': {
-        'BUNDLE_DIR_NAME': 'bundles/',
-        'STATS_FILE': os.path.join(BASE_DIR, 'webpack-stats-prd.json'),
+    "DEFAULT": {
+        "BUNDLE_DIR_NAME": "bundles/",
+        "STATS_FILE": os.path.join(BASE_DIR, "webpack-stats-prod.json"),
     }
 }
 django_heroku.settings(locals())
