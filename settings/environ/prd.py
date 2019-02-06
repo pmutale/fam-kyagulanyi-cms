b32ffd0e7dac1c140b87c0cd6c58c7f22867d224
P. Mutale <peter@mutale.nl>
2019-02-06 11:09:26 +0100

    Update prd.py

--- settings/environ/prd.py
+++ settings/environ/prd.py
@@ -5,36 +5,17 @@ import dj_database_url
 
 DEBUG = False
 
-ALLOWED_HOSTS = ["kirumba.herokuapp.com", "3.8.232.46", "kirumba.org" ]
+ALLOWED_HOSTS = ["kirumba.herokuapp.com", "3.8.232.46", "kirumba.org" ]    
 
-#if os.environ.get('AWS_ENV', None):
 DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'mwebaza_enterprise',
-            'USER': 'mwebaza',
-            'PASSWORD': 'mwebaza',
-            'HOST': 'localhost',
-            'PORT': '5432',
-        }
-}   
-#    DATABASES = {
-#    	'default':
-#	    read_pgpass(
-#            	'mwebaza_enterprise',
-#            	env='prd',
-#            	host='localhost',
-#            	engine='django.db.backends.postgresql_psycopg2')	
-#    }
-#else: 
- #   DATABASES = {
-#         "default": dj_database_url.config(default=os.environ.get("POSTGRES_URL"), conn_max_age=600, ssl_require=True),
-  #  }
-   # DATABASES["default"]["ENGINE"] = "django.db.backends.postgresql_psycopg2"
+         "default": dj_database_url.config(default=os.environ.get("POSTGRES_URL"), conn_max_age=600, ssl_require=True),
+}
 
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, "staticfiles")
-# ]
+DATABASES["default"]["ENGINE"] = "django.db.backends.postgresql_psycopg2"
+
+STATICFILES_DIRS = [
+     os.path.join(BASE_DIR, "staticfiles")
+]
 
 APPEND_SLASH = False
 
